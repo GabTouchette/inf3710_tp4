f7e9863888057c849016217cdc720cc13b3d6d2b
GabTouchette <78233945+GabTouchette@users.noreply.github.com>
2022-04-19 20:11:15 -0400

    ajout css hmtl page recherche

--- client/src/app/search-page/search.component.ts
+++ client/src/app/search-page/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-// import { PlantType } from "../../../../common/interfaces/plant-variety.interface";
+import { Plant } from "../../../../common/interfaces/plant.interface";
 
 @Component({
   selector: "app-gardens",
@@ -7,18 +7,190 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./search.component.css"],
 })
 export class Search implements OnInit {
-searchFilter: string = '';
-plantTypes = ['fsdnv', 'khjfb', 'b nwfs', 'sdfas', 'asfdgdp'];
-plantsWithFilter: string[];
+  searchFilter: string = "";
+  plants: Plant[];
+  plantsWithFilter: Plant[];
 
   public constructor() {}
 
   public ngOnInit(): void {
-    this.plantsWithFilter = this.plantTypes;
+    this.plants = [];
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plants.push({
+      name: "pomier",
+      latinName: "Pomius Arbus",
+      category: "plante a fruits",
+      type: "",
+      subType: "",
+      plantType: "Arbre",
+      menaces: [
+        {
+          description: "ratons qui mangent les pommes",
+          solutions: "poison contre les ratons",
+        },
+      ],
+    });
+    this.plantsWithFilter = this.plants;
   }
 
   public applySearchFilter() {
-    if(this.searchFilter !== '') this.plantsWithFilter = this.plantTypes.filter((ele) => ele.includes(this.searchFilter));
-    else this.plantsWithFilter = this.plantTypes;
+    if (this.searchFilter !== "") {
+      this.plantsWithFilter = this.plants.filter((plant: Plant) =>
+        plant.name.includes(this.searchFilter)
+      );
+    } else this.plantsWithFilter = this.plants;
   }
 }
